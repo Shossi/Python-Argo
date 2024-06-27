2cd45d842e3f263e17dde36c942cfe5033d289c3
yossi.shalumov <yosefshalumov.gmail.com>
2024-06-27 12:41:16 +0300

    feat: breaking change [MINOR]

--- src/app.py
+++ src/app.py
@@ -28,7 +28,7 @@ BG_COLOR = os.getenv('BG_COLOR', '#ffffff')  # default to white if not set
 
 
 def save_search_query(location, data):
-    print("hellso, awaaorldY!")
+    print("hellsao, awaaorldY!")
     if not os.path.exists(SAVE_DIR):
         os.makedirs(SAVE_DIR)
     filename = os.path.join(SAVE_DIR, f"{location}.json")
